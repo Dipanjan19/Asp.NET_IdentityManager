b8b60aab10a0f0abca29a031cc1dc73cd69e6b6b
Dipanjan19 <53527762+Dipanjan19@users.noreply.github.com>
2021-08-20 22:34:00 +0530

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,115 @@
+﻿using IdentityManager.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+     
+        [HttpGet]
+        public async Task<IActionResult> Register(string returnurl=null)
+        {
+            ViewData["ReturnUrl"] = returnurl;
+            RegisterViewModel regmodel = new RegisterViewModel();
+            return View(regmodel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model, string returnurl=null) {
+            ViewData["ReturnUrl"] = returnurl;
+            returnurl = returnurl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction(returnurl);
+                }
+
+                AddErrors(result);
+
+            }
+            return View(model);
+        }
+
+        private void AddErrors(IdentityResult result) {
+            foreach (var error in result.Errors) { 
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+
+
+        [HttpGet]
+        public IActionResult Login(string returnurl = null)
+        {
+            ViewData["ReturnUrl"] = returnurl;
+          
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model , string returnurl = null )
+        {
+            ViewData["ReturnUrl"] = returnurl;
+            returnurl = returnurl ?? Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.Rememberme, lockoutOnFailure:false);
+                if (result.Succeeded)
+                {
+                    return LocalRedirect(returnurl);
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid Login attempt or crendentials");
+                    return View(model);
+                }
+
+            }
+
+            return View(model);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logoff(RegisterViewModel model)
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+
+
+    }
+}
